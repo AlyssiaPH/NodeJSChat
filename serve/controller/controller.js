55624b249c60b3a0c5bebee159e9d1ab11a0d212
teddy Villars <teddy.villars@inetum.com>
2021-12-02 11:47:37 +0100

    create user

--- serve/controller/controller.js
+++ serve/controller/controller.js
@@ -9,7 +9,27 @@ const {response, request} = require("express");
  *
  * user : /user
  * message : /message
- *room : /room:id
+ * room : /room:id
+ *
+ *
+ * user = {
+ *     email : test@test.com ( String )
+ *     name : toto ( String )
+ *     password: test ( String )
+ *     admin : false ( Boolean )
+ *     rooms : ['test'] ( Array )
+ * }
+ *
+ * room = {
+ *     users : ['idtoto'] ( Array )
+ *     allMessages : [message15] ( Array )
+ * }
+ *
+ * message = {
+ *     idroom : ['idroom'] ( Array )
+ *     iduser : 'idtoto' ( String )
+ *     content : 'hello les gens ' ( String )
+ * }
  *
  */
 const app = express();
@@ -18,7 +38,7 @@ app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get('/',(request,reponse)=>{
+app.get('/', (request, reponse) => {
     reponse.send('hello')
 })
 
@@ -34,39 +54,95 @@ app.post('/inscription', async (request, response, next) => {
     response.send(valide);
 });
 
+/** */
 app.get('/inscription', async (request, response) => {
     console.log('Get')
     let json = {
-        email: request.query.email,
+        email: request.query.user.id,
     }
-    if(await getUser(json) === 1){
-        response.json({connect:'success'})
-    }else{
-        response.json({connect:'error'})
+    if (await getUser(json) === 1) {
+        response.json({connect: 'success'})
+    } else {
+        response.json({connect: 'error'})
     }
 });
 
 /**
  * @param: /user ( GET )
  * this path api server using to call methodes
+ * Receive => user = {
+ *     email : test@test.com ( String )
+ *     name : toto ( String )
+ *     password: test ( String )
+ *     admin : false ( Boolean )
+ *     rooms : ['test'] ( Array )
+ * }
  */
-app.get('/user',async (request, response) => {
+app.get('/user', async (request, response) => {
     console.log('Get user : ')
     console.log(request)
-    // let userGetting = await getUser(request)
-    // if (userGetting.length < 1) {
-    //     response.json({
-    //         result : null,
-    //         error : 'This user doesn\'t exist'
-    //     })
-    // }else{
-    //     response.json({
-    //         result : userGetting[0],
-    //         error : -1
-    //     })
-    // }
+    try {
+        let userGetting = await getUser(request)
+        if (userGetting.length < 1) {
+            response.json({
+                result : null,
+                error : 'This user doesn\'t exist'
+            })
+        }else{
+            response.json({
+                result : userGetting[0],
+                error : -1
+            })
+        }
+    }catch (exception){
+        response.json({
+            result : null,
+            error : 'This user doesn\'t exist'
+        })
+    }
 });
+
+/**
+ * @param: /user ( POST )
+ * @function : Create USERS
+ * this path api server using to call methodes createUsers
+ * Receive => user = {
+ *     email : test@test.com ( String )
+ *     name : toto ( String )
+ *     password: test ( String )
+ *     admin : false ( Boolean )
+ *     rooms : ['test'] ( Array )
+ * }
+ */
+app.post('/user', async (request, response) => {
+    console.log('Post created User')
+    let json = {
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password,
+        admin: request.body.admin,
+        rooms : request.body.rooms
+    }
+    console.log(json)
+    let valide = await createUser(json)
+    response.send(valide);
+})
 
 app.listen(3000, () => {
     console.log("Started on PORT 3000");
 })
+
+/**
+ *
+ * deleteuser
+ * updateuser
+ *
+ * getmessage
+ * deletemessage
+ * updatemessage
+ *
+ * getroom
+ * deleteroom
+ *
+ *
+ */
--- serve/services/service.js
+++ serve/services/service.js
@@ -2,48 +2,45 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://groupe6:ekwHcv6ZmRVRNBrSX5w@94.130.108.19/groupe6');
 
-let modelUsers = mongoose.model('user', {
+let modelUsers = mongoose.model('user',
+    {
         name: String,
         email: String,
         password: String,
-        admin: Boolean
+        admin: Boolean,
+        rooms: Array
     }
 )
-const GETISNULL = 'emailNull'
-const GETEXIST = 'emailExist'
 
-async function createUser(user) {
-    const data = new modelUsers({
-        email: user.email,
-        name:user.email,
-        password:user.password,
-        admin:user.admin
-    })
-    await data.save()
-    console.log('user : '+JSON.stringify(user)+' created !')
+async function createUser(json) {
+    try{
+        const data = new modelUsers({
+            name: json.name,
+            email: json.email,
+            password: json.password,
+            admin: json.admin,
+            rooms : json.rooms
+        })
+        await data.save()
+        let returnValue = 'user : ' + JSON.stringify(json) + ' created !'
+        console.log(returnValue)
+        return returnValue
+    }catch (exception){
+        return exception
+    }
 }
 
 async function getUser(user) {
     let data = await modelUsers.find({
         email: user.email,
-        name:user.email,
     })
-    if(data.length !== 1){
+    if (data.length !== 1) {
         return -1
-    }else{
+    } else {
         return 1
     }
 }
 
-function postUser(user) {
-
-}
-
-
-
-function deleteUser() {
-
-}
 
 module.exports = {
     createUser,
