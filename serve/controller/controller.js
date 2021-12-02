92330c5cf8d4178c4590f2f9166a0194558dcdc5
teddy Villars <teddy.villars@inetum.com>
2021-12-02 10:45:40 +0100

    server testing

--- serve/controller/controller.js
+++ serve/controller/controller.js
@@ -3,13 +3,24 @@ const cors = require('cors')
 const {createUser, getUser} = require('../services/service')
 const bodyParser = require("body-parser");
 const {get} = require("mongoose");
+const {response, request} = require("express");
 
-
+/**
+ *
+ * user : /user
+ * message : /message
+ *room : /room:id
+ *
+ */
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+
+app.get('/',(request,reponse)=>{
+    reponse.send('hello')
+})
 
 app.post('/inscription', async (request, response, next) => {
     console.log('post')
@@ -33,6 +44,27 @@ app.get('/inscription', async (request, response) => {
     }else{
         response.json({connect:'error'})
     }
+});
+
+/**
+ * @param: /user ( GET )
+ * this path api server using to call methodes
+ */
+app.get('/user',async (request, response) => {
+    console.log('Get user : ')
+    console.log(request)
+    // let userGetting = await getUser(request)
+    // if (userGetting.length < 1) {
+    //     response.json({
+    //         result : null,
+    //         error : 'This user doesn\'t exist'
+    //     })
+    // }else{
+    //     response.json({
+    //         result : userGetting[0],
+    //         error : -1
+    //     })
+    // }
 });
 
 app.listen(3000, () => {
