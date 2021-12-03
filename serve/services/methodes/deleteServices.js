8706f8a89239708027250a1ebd381b75fb0b9eff
ququelpinso <qvaudeleau@gmail.com>
2021-12-03 11:10:40 +0100

    delete vreussite
    quququuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu el pinsssssssssssssssssssooooooooooooooooo feu feu feu pah pah pah pah hiha

--- serve/services/methodes/deleteServices.js
+++ serve/services/methodes/deleteServices.js
@@ -2,7 +2,7 @@ const {mode} = require("vue-socket.io/webpack.config");
 
 async function deleteUserService(id, modelUsers,req,res) {
     try{
-        let test = await modelUsers.deleteOne({ _id: id });
+        let test = await modelUsers.findOneAndDelete({ _id: id });
         // console.log(modelUsers.findOne({_id:id}).)
         // modelUsers.findOneAndDelete({_id:id});
         // modelUsers.splice(projectIndex, 1);
